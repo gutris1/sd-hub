45b0b10af5d5daa2d1b1c0c8a54a5eefad619ff2
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-10-29 19:12:03 +0700

    4.9.2

--- sd_hub/version.py
+++ sd_hub/version.py
@@ -1,4 +1,4 @@
-version = "4.9.1"
+version = "4.9.2"
 
 import os, sys
 from pathlib import Path
@@ -6,10 +6,7 @@ from typing import List
 
 def xyz(y: str) -> List[str]:
     if 'COLAB_JUPYTER_TRANSPORT' in os.environ:
-        try:
-            x = Path(sys.executable).parent
-        except FileNotFoundError:
-            x = Path('/usr/local/bin')
+        x = Path('/usr/local/bin')
     else:
         x = Path(sys.executable).parent
 
