1675049001e48392221e70171f05940310be138d
gutris1 <132797949+gutris1@users.noreply.github.com>
2024-12-24 20:09:44 +0700

    d

--- sd_hub/paths.py
+++ sd_hub/paths.py
@@ -4,49 +4,79 @@ from modules.shared import cmd_opts
 from pathlib import Path
 import os
 
-Root = Path(data_path)
-Models = Path(models_path)
+BLOCK = (
+    "Downloading files outside of Models or Embeddings folder is blocked "
+    "\nAdd --enable-insecure-extension-access command line argument to proceed at your own risk."
+)
 
-def SDHubPath():
-    p = TagsAndPath()
-    pd = {}
+class SDPaths:
+    root = Path(data_path)
+    models = Path(models_path)
 
-    for d, fp in p:
-        pd[d.lower()] = fp
-        
-    return pd
-  
-def TagsAndPath():
-    tags_list = {
-        "$ckpt": cmd_opts.ckpt_dir or model_path,
-        "$lora": cmd_opts.lora_dir,
-        "$vae": cmd_opts.vae_dir or Models / 'VAE',
-        "$emb": cmd_opts.embeddings_dir,
-        "$ups": cmd_opts.esrgan_models_path or Models / 'ESRGAN',
-        "$cn": cmd_opts.controlnet_dir or Models / 'ControlNet',
-        "$hn": cmd_opts.hypernetwork_dir or Models / 'hypernetworks',
-        "$ad": Models / 'adetailer',
-        "$cf": cmd_opts.codeformer_models_path or Models / 'Codeformer'
-    }
+    ckpt_dir = cmd_opts.ckpt_dir or model_path
+    lora_dir = cmd_opts.lora_dir
+    vae_dir = cmd_opts.vae_dir or models / "VAE"
+    embeddings_dir = cmd_opts.embeddings_dir
+    esrgan_dir = cmd_opts.esrgan_models_path or models / "ESRGAN"
+    controlnet_dir = cmd_opts.controlnet_dir or models / "ControlNet"
+    hypernetwork_dir = cmd_opts.hypernetwork_dir or models / "hypernetworks"
+    ad_model = models / "adetailer"
+    codeformer_dir = cmd_opts.codeformer_models_path or models / "Codeformer"
+    extensions_dir = extensions_dir
 
-    paths = []
+    @staticmethod
+    def SDHubTagsAndPaths():
+        tags_list = {
+            "$ckpt": SDPaths.ckpt_dir,
+            "$lora": SDPaths.lora_dir,
+            "$vae": SDPaths.vae_dir,
+            "$emb": SDPaths.embeddings_dir,
+            "$ups": SDPaths.esrgan_dir,
+            "$cn": SDPaths.controlnet_dir,
+            "$hn": SDPaths.hypernetwork_dir,
+            "$ad": SDPaths.ad_model,
+            "$cf": SDPaths.codeformer_dir
+        }
 
-    for t, d in tags_list.items():
-        paths.append([t, str(d)])
+        paths = [[t, str(d)] for t, d in tags_list.items()]
 
-    e = {
-        'COLAB_JUPYTER_TRANSPORT': '/content',
-        'SAGEMAKER_INTERNAL_IMAGE_URI': '/home/studio-lab-user',
-        'KAGGLE_DATA_PROXY_TOKEN': '/kaggle/working'
-    }
+        if cmd_opts.enable_insecure_extension_access:
+            paths.append(["$ext", str(SDPaths.extensions_dir)])
+            paths.append(["$root", str(SDPaths.root)])
 
-    if cmd_opts.enable_insecure_extension_access:
-        paths.append(["$ext", extensions_dir])
-        paths.append(["$root", Root])
+            env_list = {
+                "COLAB_JUPYTER_TRANSPORT": "/content",
+                "SAGEMAKER_INTERNAL_IMAGE_URI": "/home/studio-lab-user",
+                "KAGGLE_DATA_PROXY_TOKEN": "/kaggle/working"
+            }
 
-        for v, p in e.items():
-            if v in os.environ:
-                paths.append(["$home", p])
-                break
+            for var, path in env_list.items():
+                if var in os.environ:
+                    paths.append(["$home", path])
+                    break
 
-    return paths
+        return paths
+
+    @staticmethod
+    def SDHubPaths():
+        paths = SDPaths.SDHubTagsAndPaths()
+        return {tag.lower(): path for tag, path in paths}
+
+    def SDHubCheckPaths(self, inputs):
+        target = Path(inputs).resolve()
+        allowed_dirs = [
+            Path(self.ckpt_dir).resolve(),
+            Path(self.lora_dir).resolve(),
+            Path(self.vae_dir).resolve(),
+            Path(self.embeddings_dir).resolve(),
+            Path(self.esrgan_dir).resolve(),
+            Path(self.controlnet_dir).resolve(),
+            Path(self.hypernetwork_dir).resolve(),
+            Path(self.ad_model).resolve(),
+            Path(self.codeformer_dir).resolve(),
+        ]
+
+        if not any(dirs in target.parents or target == dirs for dirs in allowed_dirs):
+            return False, f"{target}\n{BLOCK}"
+
+        return True, ""
