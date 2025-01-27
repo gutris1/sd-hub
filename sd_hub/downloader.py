74f982722fbb718cde01d83445b29b43d705c58f
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-01-27 16:36:00 +0700

    Update downloader.py

--- sd_hub/downloader.py
+++ sd_hub/downloader.py
@@ -1,9 +1,14 @@
+from modules.scripts import basedir
+from modules.shared import cmd_opts
 from urllib.parse import urlparse
 from pathlib import Path
-from modules.shared import cmd_opts
-from modules.scripts import basedir
 import gradio as gr
-import subprocess, re, sys, requests, time, shlex
+import subprocess
+import requests
+import shlex
+import time
+import sys
+import re
 
 from sd_hub.paths import SDHubPaths, BLOCK
 from sd_hub.version import xyz
@@ -11,15 +16,51 @@ from sd_hub.version import xyz
 tag_tag = SDHubPaths.SDHubTagsAndPaths()
 aria2cexe = Path(basedir()) / 'aria2c.exe'
 
+def gitclown(url, target_path):
+    parts = shlex.split(url)
+    repo = parts[0]
+
+    cmd = ['git', 'clone', repo]
+    cwd = str(target_path)
+
+    if len(parts) > 1:
+        cmd.extend(parts[1:])
+
+    p = subprocess.Popen(
+        cmd,
+        cwd=cwd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        bufsize=1,
+        text=True
+    )
+
+    git_output = ''
+
+    while True:
+        output = p.stdout.readline()
+        if not output:
+            break
+        git_output += output
+
+        for lines in output.splitlines():
+            yield lines, False
+
+    for lines in git_output.splitlines():
+        yield lines, True
+
+    p.wait()
+
+
 def gdrown(url, target_path=None, fn=None):
-    gfolder = "drive.google.com/drive/folders" in url
+    gfolder = 'drive.google.com/drive/folders' in url
     cli = xyz('gdown.exe') if sys.platform == 'win32' else xyz('gdown')
-    cmd = cli + ["--fuzzy", url]
+    cmd = cli + ['--fuzzy', url]
 
     if fn:
-        cmd += ["-O", fn]
+        cmd += ['-O', fn]
     if gfolder:
-        cmd.append("--folder")
+        cmd.append('--folder')
 
     cwd = target_path if target_path else Path.cwd()
 
@@ -32,7 +73,7 @@ def gdrown(url, target_path=None, fn=None):
         text=True
     )
 
-    gdown_output = ""
+    gdown_output = ''
     gdown_progress = None
     starting_line = time.time()
     failure = False
@@ -43,7 +84,7 @@ def gdrown(url, target_path=None, fn=None):
             break
         gdown_output += output
 
-        if "Failed to retrieve file url" in output:
+        if 'Failed to retrieve file url' in output:
             failure = True
 
         if re.search(r'\d{1,3}%', output):
@@ -54,77 +95,81 @@ def gdrown(url, target_path=None, fn=None):
             starting_line = time.time()
 
     if failure:
-        failed = gdown_output.find("Failed to retrieve file url")
+        failed = gdown_output.find('Failed to retrieve file url')
         lines = gdown_output[failed:]
         yield lines, False
-    
+
     for lines in gdown_output.split('\n'):
-        if lines.startswith("To:"):
+        if lines.startswith('To:'):
             completed = re.search(r'[^/]*$', lines)
             if completed:
-                yield f"Saved To: {target_path}/{completed.group()}", True
+                yield f'Saved To: {target_path}/{completed.group()}', True
+
     p.wait()
 
 
 def ariari(url, target_path=None, fn=None, token2=None, token3=None):
     aria2cmd = [aria2cexe] if sys.platform == 'win32' else xyz('aria2c')
 
-    if "huggingface.co" in url:
-        if "/blob/" in url:
-            url = url.replace("/blob/", "/resolve/")
+    if any(domain in url for domain in ['huggingface.co', 'gitHub.com']):
+        if '/blob/' in url:
+            if 'gitHub.com' in url:
+                url = url.replace('/blob/', '/raw/')
+            else:
+                url = url.replace('/blob/', '/resolve/')
 
-        if token2:
-            aria2cmd.extend(["--header=User-Agent: Mozilla/5.0",
-                             f"--header=Authorization: Bearer {token2}"])
+        if 'huggingface.co' in url and token2:
+            aria2cmd.extend(['--header=User-Agent: Mozilla/5.0',
+                            f'--header=Authorization: Bearer {token2}'])
 
-    elif "civitai.com" in url:
+    elif 'civitai.com' in url:
         if token3:
-            aria2cmd.extend(["--header=User-Agent: Mozilla/5.0"])
+            aria2cmd.extend(['--header=User-Agent: Mozilla/5.0'])
 
             url = url.split('?token=')[0] if '?token=' in url else url
             if '?type=' in url:
                 url = url.replace('?type=', f'?token={token3}&type=')
             else:
-                url = f"{url}?token={token3}"
+                url = f'{url}?token={token3}'
 
-        if "civitai.com/models/" in url:
+        if 'civitai.com/models/' in url:
             try:
                 model_id, version_id = (url.split('/models/')[1].split('/')[0], None)
                 if '?modelVersionId=' in url:
                     version_id = url.split('?modelVersionId=')[1]
-                    response = requests.get(f"https://civitai.com/api/v1/model-versions/{version_id}")
+                    response = requests.get(f'https://civitai.com/api/v1/model-versions/{version_id}')
                 else:
-                    response = requests.get(f"https://civitai.com/api/v1/models/{model_id}")
+                    response = requests.get(f'https://civitai.com/api/v1/models/{model_id}')
 
                 response.raise_for_status()
                 data = response.json()
 
-                early_access = data.get("earlyAccessEndsAt")
+                early_access = data.get('earlyAccessEndsAt')
                 if early_access:
-                    model_page = f"https://civitai.com/models/{data.get('modelId')}?modelVersionId={data.get('id')}"
-                    msg = f"The model is in early access and requires payment for downloading."
-                    yield f"-> {model_page}\n{msg}", False
+                    model_page = f'https://civitai.com/models/{data.get("modelId")}?modelVersionId={data.get("id")}'
+                    msg = f'The model is in early access and requires payment for downloading.'
+                    yield f'-> {model_page}\n{msg}', False
                     return
 
-                download_url = data.get("downloadUrl") or data.get("modelVersions", [{}])[0].get("downloadUrl", "")
+                download_url = data.get('downloadUrl') or data.get('modelVersions', [{}])[0].get('downloadUrl', '')
                 if not download_url:
-                    yield f"Unable to find download URL for\n-> {url}\n", False
+                    yield f'Unable to find download URL for\n-> {url}\n', False
                     return
 
-                url = f"{download_url}?token={token3}" if token3 else download_url
+                url = f'{download_url}?token={token3}' if token3 else download_url
             except requests.exceptions.RequestException as e:
-                yield f"{str(e)}\n", True
+                yield f'{str(e)}\n', True
                 return
 
     aria2cmd.extend([
-        "--console-log-level=error",
-        "--stderr=true",
-        "--summary-interval=1",
-        "-c", 
-        "-x16", 
-        "-s16", 
-        "-k1M", 
-        "-j5"
+        '--console-log-level=error',
+        '--stderr=true',
+        '--summary-interval=1',
+        '-c', 
+        '-x16', 
+        '-s16', 
+        '-k1M', 
+        '-j5'
     ])
 
     if target_path:
@@ -134,14 +179,14 @@ def ariari(url, target_path=None, fn=None, token2=None, token3=None):
                 yield err, False
                 return
             else:
-                aria2cmd.extend(["--allow-overwrite=true"])
+                aria2cmd.extend(['--allow-overwrite=true'])
         else:
-            aria2cmd.extend(["--allow-overwrite=true"])
+            aria2cmd.extend(['--allow-overwrite=true'])
 
-        aria2cmd.extend(["-d", target_path])
+        aria2cmd.extend(['-d', target_path])
 
     if fn:
-        aria2cmd.extend(["-o", fn])
+        aria2cmd.extend(['-o', fn])
 
     aria2cmd.append(url)
 
@@ -153,7 +198,7 @@ def ariari(url, target_path=None, fn=None, token2=None, token3=None):
         text=True
     )
 
-    aria2_output = ""
+    aria2_output = ''
     break_line = False
     error = False
 
@@ -164,13 +209,13 @@ def ariari(url, target_path=None, fn=None, token2=None, token3=None):
 
         aria2_output += output
 
-        if "errorCode=24" in output:
+        if 'errorCode=24' in output:
             uri_pattern = re.search(r'URI=(https?://\S+)', output)
             if uri_pattern:
                 uri = uri_pattern.group(1)
                 url_list = {
-                    "huggingface.co": f"## Authorization Failed, Enter your Huggingface Token\n-> {url}\n",
-                    "civitai.com": f"## Authorization Failed, Enter your Civitai API Key\n-> {url}\n"
+                    'huggingface.co': f'## Authorization Failed, Enter your Huggingface Token\n-> {url}\n',
+                    'civitai.com': f'## Authorization Failed, Enter your Civitai API Key\n-> {url}\n'
                 }
 
                 for domain, msg in url_list.items():
@@ -180,10 +225,10 @@ def ariari(url, target_path=None, fn=None, token2=None, token3=None):
                         break
             continue
 
-        if "errorCode" in output:
-            arrow = aria2_output.find("->")
+        if 'errorCode' in output:
+            arrow = aria2_output.find('->')
             if arrow != -1:
-                lines = aria2_output.find("\n", arrow)
+                lines = aria2_output.find('\n', arrow)
                 if lines != -1:
                     errorcode = aria2_output[arrow:lines]
                     uri_pattern = re.search(r'URI=(https?://\S+)', aria2_output)
@@ -201,7 +246,7 @@ def ariari(url, target_path=None, fn=None, token2=None, token3=None):
                 sizes = dl_line.group(1)
                 percent = dl_line.group(2)
                 speed = dl_line.group(3)
-                outputs = f"{percent} | {sizes} | {speed}/s"
+                outputs = f'{percent} | {sizes} | {speed}/s'
                 yield outputs, False
 
                 break_line = True
@@ -212,142 +257,125 @@ def ariari(url, target_path=None, fn=None, token2=None, token3=None):
         pass
 
     if not error:
-        separator = aria2_output.find("======+====+===========")
+        separator = aria2_output.find('======+====+===========')
         if separator != -1:
             for lines in aria2_output[separator:].splitlines():
-                if "|" in lines:
+                if '|' in lines:
                     pipe = lines.split('|')
                     if len(pipe) > 3:
                         output_dir = pipe[3].strip()
-                        yield f"Saved To: {output_dir}", True
+                        yield f'Saved To: {output_dir}', True
+
     p.wait()
+
 
 def get_fn(url):
     fn_fn = urlparse(url)
 
-    if "civitai.com" in fn_fn.netloc or "drive.google.com" in fn_fn.netloc:
+    if 'civitai.com' in fn_fn.netloc or 'drive.google.com' in fn_fn.netloc:
         return None
     else:
         fn = Path(fn_fn.path).name
         return fn
+
 
 def url_check(url):
     try:
         url_parsed = urlparse(url)
 
         if not (url_parsed.scheme and url_parsed.netloc):
-            return False, "Invalid URL."
+            return False, 'Invalid URL.'
 
         supported = [
-            "civitai.com",
-            "huggingface.co",
-            "github.com",
-            "drive.google.com"
+            'civitai.com',
+            'huggingface.co',
+            'github.com',
+            'drive.google.com'
         ]
 
         if url_parsed.netloc not in supported:
-            supported_str = "\n".join(supported)
-            return False, f"Supported Domain :\n{supported_str}"
+            supported_str = '\n'.join(supported)
+            return False, f'Supported Domain :\n{supported_str}'
 
-        return True, ""
+        return True, ''
     except Exception as e:
         return False, str(e)
 
-def input_process(url_line, current_path):
+
+def process_inputs(url_line, current_path, ext_tag, github_repo):
     if any(url_line.startswith(char) for char in ('/', '\\', '#')):
-        return None, None, None, "Invalid usage, Tag should start with $"
+        return None, None, None, 'Invalid usage, Tag should start with $'
 
     if url_line.startswith('$'):
         parts = url_line[1:].strip().split('/', 1)
-        tags_key = f"${parts[0].lower()}"
+        tags_key = f'${parts[0].lower()}'
         subfolder = parts[1] if len(parts) > 1 else None
         base_path = tag_tag.get(tags_key)
         if base_path is not None:
             full_path = Path(base_path, subfolder) if subfolder else Path(base_path)
             current_path = full_path
         else:
-            return None, None, None, f"{tags_key}\nInvalid Tag."
+            return None, None, None, f'{tags_key}\nInvalid Tag.'
         return current_path, None, None, None
 
     parts = shlex.split(url_line)
     url = parts[0].strip()
 
-    is_valid, error_message = url_check(url)
+    is_valid, err = url_check(url)
     if not is_valid:
-        return None, None, None, error_message
+        return None, None, None, err
 
     optional_path = None
     optional_fn = None
 
     if len(parts) > 1:
-        if '-' in parts:
-            dash_index = parts.index('-')
-            optional_path_raw = ' '.join(parts[1:dash_index]).strip()
-            optional_fn = ' '.join(parts[dash_index + 1:]).strip()
+        if ext_tag and github_repo:
+            url = ' '.join(parts).strip()
         else:
-            optional_path_raw = ' '.join(parts[1:]).strip()
+            if '-' in parts:
+                dash_index = parts.index('-')
+                optional_path_raw = ' '.join(parts[1:dash_index]).strip()
+                optional_fn = ' '.join(parts[dash_index + 1:]).strip()
+            else:
+                optional_path_raw = ' '.join(parts[1:]).strip()
 
-        optional_path_raw = optional_path_raw.strip('"').strip("'")
-        optional_path = Path(optional_path_raw) if optional_path_raw else None
+            optional_path_raw = optional_path_raw.strip('"').strip("'")
+            optional_path = Path(optional_path_raw) if optional_path_raw else None
 
     if optional_path and optional_path.suffix:
-        return None, None, None, f"{optional_path}\nOutput path is not a path."
+        return None, None, None, f'{optional_path}\nOutput path is not a path.'
 
     if optional_fn:
         optional_fn_path = Path(optional_fn)
         if not optional_fn_path.suffix:
-            return None, None, None, f"{optional_fn}\nOutput filename is missing its extension."
+            return None, None, None, f'{optional_fn}\nOutput filename is missing its extension.'
 
     target_path = optional_path if optional_path else current_path
     if target_path is None or not target_path.exists():
-        return None, None, None, f"{target_path}\nDoes not exist."
+        return None, None, None, f'{target_path}\nDoes not exist.'
 
     fn = get_fn(url) if not optional_fn else optional_fn
 
     return target_path, url, fn, None
 
-def git_clown(url, target_path, subfolder=None):
-    clone_path = target_path
 
-    if subfolder:
-        clone_path = Path(target_path, subfolder)
-
-    process = subprocess.Popen(
-        ['git', 'clone', url],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-        cwd=str(clone_path)
-    )
-
-    while True:
-        output = process.stdout.readline()
-        if output == '' and process.poll() is not None:
-            break
-        if output:
-            yield output.strip(), True
-
-    if process.returncode != 0:
-        error = process.stderr.read()
-        yield f"{error}", True
-
-def surface(command, token2=None, token3=None):
+def lobby(command, token2=None, token3=None):
     if not command.strip():
-        yield "Nothing To See Here.", True
+        yield 'Nothing To See Here.', True
         return
 
     current_path = None
     urls = [url_line for url_line in command.strip().split('\n') if url_line.strip()]
 
     if len(urls) == 1 and urls[0].startswith('$'):
-        yield "Missing URL.", True
+        yield 'Missing URL.', True
         return
 
     ext_tag = urls[0].startswith('$ext')
     github_repo = any('github.com' in url_line and not Path(urlparse(url_line).path).suffix for url_line in urls)
 
     for url_line in urls:
-        target_path, url, fn, error = input_process(url_line, current_path)
+        target_path, url, fn, error = process_inputs(url_line, current_path, ext_tag, github_repo)
 
         if error:
             yield error, True
@@ -357,12 +385,13 @@ def surface(command, token2=None, token3=None):
             current_path = target_path
             continue
 
-        if github_repo and ext_tag:
-            for msg, err in git_clown(url, target_path):
-                yield msg, err
-            continue
+        if ext_tag and github_repo:
+            if cmd_opts.enable_insecure_extension_access:
+                for msg, err in gitclown(url, target_path):
+                    yield msg, err
+                continue
 
-        if "drive.google" in url:
+        if 'drive.google' in url:
             for msg, err in gdrown(url, target_path, fn):
                 yield msg, err
             continue
@@ -381,26 +410,27 @@ def downloader(command, token2, token3, box_state=gr.State()):
     output_box = box_state if box_state else []
 
     ngword = [
-        "## Authorization Failed",
-        "The model is in early access",
-        "Unable to find",
-        "errorCode",
-        "Failed to retrieve"
+        '## Authorization Failed',
+        'The model is in early access',
+        'Unable to find',
+        'errorCode',
+        'Failed to retrieve',
+        'fatal:'
     ]
 
-    yield "Now Downloading...", ""
+    yield 'Now Downloading...', ''
 
-    for _text, _flag in surface(command, token2, token3):
+    for _text, _flag in lobby(command, token2, token3):
         if not _flag:
             if any(k in _text for k in ngword):
-                yield "Error", "\n".join([_text] + output_box)
+                yield 'Error', '\n'.join([_text] + output_box)
                 return gr.update(), gr.State(output_box)
 
-            if "files from/to outside" in _text: 
-                yield "Blocked", "\n".join([_text] + output_box)
+            if 'files from/to outside' in _text: 
+                yield 'Blocked', '\n'.join([_text] + output_box)
                 assert not cmd_opts.disable_extension_access, BLOCK
 
-            yield _text, "\n".join(output_box)
+            yield _text, '\n'.join(output_box)
         else:
             output_box.append(_text)
 
@@ -410,23 +440,24 @@ def downloader(command, token2, token3, box_state=gr.State()):
     ]
 
     if any(k in w for k in catcher for w in output_box):
-        yield "Error", "\n".join(output_box)
+        yield 'Error', '\n'.join(output_box)
     else:
-        yield "Done", "\n".join(output_box)
+        yield 'Done', '\n'.join(output_box)
 
     return gr.update(), gr.State(output_box)
+
 
 def read_txt(file, box):
     text_box = []
 
     if file is not None:
         inputs = file.name
-        txt = ""
+        txt = ''
 
         with open(inputs, 'r') as content:
             txt = content.read()
 
-        if box.strip() == "":
+        if box.strip() == '':
             result = txt
         else:
             result = box + '\n' + txt
