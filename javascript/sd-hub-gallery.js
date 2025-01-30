f4644d9cd0191cc271e4d3302f617ad270020f8d
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-01-30 21:12:46 +0700

    Update sd-hub-gallery.js

--- javascript/sd-hub-gallery.js
+++ javascript/sd-hub-gallery.js
@@ -1,4 +1,4 @@
-function LoadInitalGallery() {
+function GalleryInitialLoad() {
   let imgPathList = document.querySelector('#sdhub-gallery-imginitial textarea');
 
   if (imgPathList && imgPathList.value.trim() !== '') {
@@ -58,6 +58,66 @@ function LoadInitalGallery() {
   }
 }
 
+function LoadGallery() {
+  fetch('/sd-hub-gallery-initial')
+    .then(response => response.json())
+    .then(data => {
+      const PathList = data.image_paths;
+      let DivIndex = 1;
+      let imgDiv = document.querySelector('#sdhub-imgdiv-0');
+
+      PathList.forEach((imgPathValue) => {
+        let imgPath = imgPathValue.replace(/[\[\]']+/g, '').trim();
+        let whichTab = Tabname.find(whichTab => imgPath.includes(whichTab));
+
+        if (whichTab) {
+          let TabDiv = document.getElementById(`sdhub-gallery-${whichTab}-tab-div`);
+          let TabBtn = document.getElementById(`sdhub-gallery-${whichTab}-tab-button`);
+
+          if (imgDiv && TabDiv) {
+            let newImgDiv = imgDiv.cloneNode(true);
+            newImgDiv.classList.add('sdhub-gallery-img-div');
+
+            let newId = `sdhub-imgdiv-${DivIndex}`;
+            while (document.getElementById(newId)) {
+              DivIndex++;
+              newId = `sdhub-imgdiv-${DivIndex}`;
+            }
+
+            newImgDiv.id = newId;
+
+            let img = newImgDiv.querySelector('img');
+            if (img) {
+              img.classList.add('sdhub-gallery-img');
+              img.src = imgPath;
+            }
+
+            TabDiv.appendChild(newImgDiv);
+
+            if (TabBtn) {
+              TabBtn.style.display = 'flex';
+            }
+
+            DivIndex++;
+          }
+        }
+      });
+
+      for (let i = 0; i < Tabname.length; i++) {
+        let TabBtn = document.getElementById(`sdhub-gallery-${Tabname[i]}-tab-button`);
+        let TabDiv = document.getElementById(`sdhub-gallery-${Tabname[i]}-tab-div`);
+
+        if (TabBtn && TabDiv) {
+          TabBtn.classList.add('selected');
+          TabDiv.classList.add('active');
+          TabDiv.style.display = 'flex';
+          break;
+        }
+      }
+    })
+    .catch(error => console.error('Error:', error));
+}
+
 const Tabname = [
   'txt2img-images',
   'img2img-images',
@@ -77,26 +137,29 @@ onUiLoaded(function () {
       }
     });
 
-    const tabContainer = document.createElement('div');
-    tabContainer.id = 'sdhub-gallery-tab-button-row';
+    const TabRow = document.createElement('div');
+    TabRow.id = 'sdhub-gallery-tab-button-row';
+
+    const btnClass = ['lg', 'primary', 'gradio-button', 'svelte-cmf5ev', 'sdhub-gallery-tab-button'];
 
     Tabname.forEach(whichTab => {
-      const buttonContent = whichTab.split('-')[0].toLowerCase();
-      const button = document.createElement('button');
-      button.id = `sdhub-gallery-${whichTab}-tab-button`;
-      button.classList.add('lg', 'primary', 'gradio-button', 'svelte-cmf5ev', 'sdhub-gallery-tab-button');
-      button.textContent = buttonContent;
-      button.addEventListener('click', () => switchGalleryTab(whichTab));
+      let btnTitle = whichTab.includes('grids') 
+        ? whichTab 
+        : whichTab.split('-')[0].toLowerCase();
 
-      const tabDiv = document.createElement('div');
-      tabDiv.id = `sdhub-gallery-${whichTab}-tab-div`;
-      tabDiv.classList.add('sdhub-gallery-tab-div');
+      const TabBtn = document.createElement('button');
+      TabBtn.id = `sdhub-gallery-${whichTab}-tab-button`;
+      TabBtn.classList.add(...btnClass);
+      TabBtn.textContent = btnTitle;
+      TabBtn.addEventListener('click', () => switchGalleryTab(whichTab));
 
-      tabContainer.appendChild(button);
-      Gallery.appendChild(tabDiv);
+      const TabDiv = document.createElement('div');
+      TabDiv.id = `sdhub-gallery-${whichTab}-tab-div`;
+      TabDiv.classList.add('sdhub-gallery-tab-div');
+
+      TabRow.append(TabBtn);
+      Gallery.append(TabDiv);
     });
-
-    Gallery.prepend(tabContainer);
 
     const imgDiv = document.createElement('div');
     imgDiv.id = 'sdhub-imgdiv-0';
@@ -108,31 +171,37 @@ onUiLoaded(function () {
     textarea.style.display = 'none';
     textarea.classList.add('prose');
 
-    const button1 = document.createElement('button');
-    const button2 = document.createElement('button');
-    const button3 = document.createElement('button');
-    const button4 = document.createElement('button');
+    const btn1 = document.createElement('button');
+    const btn2 = document.createElement('button');
+    const btn3 = document.createElement('button');
+    const btn4 = document.createElement('button');
 
-    sendbutton.append(button1, button2, button3, button4);
+    sendbutton.append(btn1, btn2, btn3, btn4);
     imgDiv.append(img, textarea, sendbutton);
-    Gallery.appendChild(imgDiv);
+    Gallery.prepend(TabRow, imgDiv);
 
-    LoadInitalGallery();
+    LoadGallery();
   }
 });
 
 function switchGalleryTab(whichTab) {
-  document.querySelectorAll('[id^="sdhub-gallery-"][id$="-tab-div"]').forEach(tab => {
-    tab.style.display = 'none';
+  document.querySelectorAll('[id^="sdhub-gallery-"][id$="-tab-div"]').forEach(Tab => {
+    Tab.style.display = 'none';
+    Tab.classList.remove('active');
   });
 
-  document.querySelectorAll('[id^="sdhub-gallery-"][id$="-tab-button"]').forEach(btn => {
-    btn.classList.remove('selected');
+  document.querySelectorAll('[id^="sdhub-gallery-"][id$="-tab-button"]').forEach(Btn => {
+    Btn.classList.remove('selected');
   });
 
   const Tab = document.getElementById(`sdhub-gallery-${whichTab}-tab-div`);
-  if (Tab) Tab.style.display = 'flex';
+  if (Tab) {
+    Tab.style.display = 'flex';
+    Tab.classList.add('active');
+  }
 
-  const Button = document.getElementById(`sdhub-gallery-${whichTab}-tab-button`);
-  if (Button) Button.classList.add('selected');
+  const Btn = document.getElementById(`sdhub-gallery-${whichTab}-tab-button`);
+  if (Btn) {
+    Btn.classList.add('selected');
+  }
 }
