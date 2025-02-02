07ac2ade1647d062d208e1ebc028709335ec54a1
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-02-02 10:33:59 +0700

    Update galleryTab.py

--- sd_hub/galleryTab.py
+++ sd_hub/galleryTab.py
@@ -8,10 +8,12 @@ from fastapi import FastAPI, Request, Response
 from fastapi.staticfiles import StaticFiles
 from urllib.parse import unquote, quote
 import sys
+from fastapi.responses import FileResponse
 
 image_extensions = ['.png', '.jpg', '.jpeg', '.webp', '.avif']
 outputs_dir = opts.outdir_samples or Path(data_path) / 'outputs'
 root_dir = str(Path(outputs_dir).anchor) if sys.platform == 'win32' else "/"
+BASE = "/sd-hub-gallery"
 
 def getimg():
     if not outputs_dir.exists():
@@ -23,47 +25,76 @@ def getimg():
         if path.suffix.lower() in image_extensions
     ]
 
-BASE = "/sd-hub-gallery"
-def hook(app: FastAPI):
+def GalleryAPI(app: FastAPI):
     app.mount(BASE, StaticFiles(directory=root_dir, html=True), name="sd-hub-gallery")
+    endpoint = '/' + req.scope.get('path', 'err').strip('/')
+
+    @app.middleware("http")
+    async def logimg(req: Request, call_next):
+        if endpoint.startswith('/file='):
+            fp = Path(unquote(endpoint[6:]))
+            if fp.suffix.lower() in image_extensions:
+                print(f"img : {fp}")
+
+        return await call_next(req)
+
+    @app.middleware("http")
+    async def loglog(req: Request, call_next):
+        method = req.method
+        print(f"Request Method: {method}, Path: {endpoint}")
+
+        fp = Path(endpoint)
+        if fp.suffix.lower() in image_extensions:
+            print(f"Image request: {fp}")
+
+        return await call_next(req)
 
     @app.get("/sd-hub-gallery-initial")
     async def initialLoad():
         image_paths = getimg()
         return {"image_paths": [
-            f"{BASE}/{quote(str(path).lstrip('/'))}" if sys.platform == 'win32' else f"{BASE}{quote(str(path))}"
+            f"{BASE}{quote(str(path).lstrip('/'))}" if sys.platform == 'win32' else f"{BASE}{quote(str(path))}"
             for path in image_paths
         ]}
 
+def GalleryGallery(_: gr.Blocks, app: FastAPI):
+    GalleryAPI(app)
+
 def SDHubGallery():
-    with gr.TabItem("Gallery", elem_id="sdhub-gallery-tab", elem_classes="tabs"):
-        #gr.HTML(elem_id="sdhub-gallery")
+    with gr.TabItem("Gallery", elem_id="sdhub-gallery-tab"):
         gr.Textbox(elem_id="sdhub-gallery-imginitial", visible=False)
-        geninfo = gr.Textbox(elem_id="sdhub-gallery-geninfo", visible=False)
-
         imgpath = gr.Textbox(elem_id="sdhub-gallery-imgpath", visible=False)
-        #imgpath.change(fn=None, _js="() => {SDHubGalleryImgPath();}")
 
-        with FormColumn(variant="compact", visible=False):
-            image = gr.Image(
-                elem_id="sdhub-gallery-img-input",
-                type="pil",
-                source="upload",
-                show_label=False
-            )
-        with FormColumn(variant="compact", visible=False, elem_id="sdhub-gallery-sendbutton-column"):
-            buttons = tempe.create_buttons(
-                ["txt2img", "img2img", "inpaint", "extras"]
-            )
-
-        for tabname, button in buttons.items():
-            tempe.register_paste_params_button(
-                tempe.ParamBinding(
-                    paste_button=button, 
-                    tabname=tabname, 
-                    source_text_component=geninfo, 
-                    source_image_component=imgpath
+        with FormRow(equal_height=False, elem_id="sdhub-gallery-image-info-row"):
+            with FormColumn(variant="compact"):
+                image = gr.Image(
+                    elem_id="SDHubimgInfoImage",
+                    type="pil",
+                    source="upload",
+                    show_label=False
                 )
-            )
 
-        image.change(fn=None, _js="() => {SDImageInfoParser();}")
+                with FormColumn(variant="compact", elem_id="SDHubimgInfoSendButton"):
+                    buttons = tempe.create_buttons(
+                        ["txt2img", "img2img", "inpaint", "extras"]
+                    )
+
+            with FormColumn(variant="compact", scale=7, elem_id="SDHubimgInfoOutputPanel"):
+                geninfo = gr.Textbox(
+                    elem_id="SDHubimgInfoGenInfo",
+                    visible=False
+                )
+
+                gr.HTML(elem_id="SDHubimgInfoHTML")
+
+            for tabname, button in buttons.items():
+                tempe.register_paste_params_button(
+                    tempe.ParamBinding(
+                        paste_button=button, 
+                        tabname=tabname, 
+                        source_text_component=geninfo, 
+                        source_image_component=image
+                    )
+                )
+
+            image.change(fn=None, _js="() => {SDHubGalleryParser();}")
