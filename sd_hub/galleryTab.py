6564ef5c3e50da9ed1e113ca302e138e3b628cb8
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-02-07 15:06:06 +0700

    Update galleryTab.py

--- sd_hub/galleryTab.py
+++ sd_hub/galleryTab.py
@@ -1,4 +1,4 @@
-import modules.generation_parameters_copypaste as tempe
+import modules.generation_parameters_copypaste as tempe # type: ignore
 from modules.ui_components import FormRow, FormColumn
 from fastapi import FastAPI, HTTPException, responses
 from modules.paths_internal import data_path
@@ -10,6 +10,7 @@ from PIL import Image
 import gradio as gr
 import tempfile
 import hashlib
+import json
 import sys
 
 imgEXT = ['.png', '.jpg', '.jpeg', '.webp', '.avif']
@@ -17,6 +18,7 @@ out_dir = opts.outdir_samples or Path(data_path) / 'outputs'
 root_dir = str(Path(out_dir).anchor) if sys.platform == 'win32' else "/"
 thumb_dir = Path(tempfile.gettempdir()) / "sd-hub-gallery-thumb"
 thumb_dir.mkdir(exist_ok=True, parents=True)
+img_list = thumb_dir / "img_list.json" 
 BASE = "/sd-hub-gallery"
 
 def getTimeStamp():
@@ -36,9 +38,18 @@ def getThumb(src: Path):
             img.convert("RGB").save(thumb_path, "JPEG", quality=85)
     return thumb_path
 
+def getImageList():
+    if img_list.exists() and img_list.stat().st_size > 0:
+        return json.loads(img_list.read_text(encoding="utf-8"))
+    return None
+
 def getImage():
     img = [p for p in Path(out_dir).rglob("*") if p.suffix.lower() in imgEXT]
-    img.sort(key=lambda p: p.stat().st_ctime)
+    if not img:
+        return []
+
+    img.sort(key=lambda p: p.stat().st_mtime)
+
     results = []
     for path in img:
         thumb_path = getThumb(path)
@@ -46,6 +57,8 @@ def getImage():
             "path": f"{BASE}/image{quote(str(path))}",
             "thumb": f"{BASE}/thumb/{quote(thumb_path.name)}"
         })
+
+    img_list.write_text(json.dumps(results, indent=4), encoding="utf-8")
     return results
 
 class GalleryState:
@@ -59,30 +72,36 @@ def GalleryGallery(app: FastAPI):
     headers = {"Cache-Control": "public, max-age=31536000"}
 
     @app.get("/sd-hub-gallery-initial")
-    async def initial():
-        imgs = getImage()
+    async def initialLoad():
+        imgs = getImageList()
+        if imgs is None:
+            imgs = getImage()
+
         Gallery.initial_list = [img["path"] for img in imgs]
         return {"images": imgs}
 
     @app.get("/sd-hub-gallery-list")
-    async def new():
+    async def newImage():
         imgs = getImage()
-        new_imgs = [img for img in imgs if img["path"] not in Gallery.initial_list]
-        if new_imgs:
-            Gallery.initial_list.extend([img["path"] for img in new_imgs])
-            Gallery.path_list = [img["path"] for img in new_imgs] + Gallery.path_list
-            return {"images": new_imgs}
+        new = [img for img in imgs if img["path"] not in Gallery.initial_list]
+
+        if new:
+            Gallery.initial_list.extend([img["path"] for img in new])
+            Gallery.path_list = [img["path"] for img in new] + Gallery.path_list
+            img_list.write_text(json.dumps(imgs, indent=4), encoding="utf-8")
+
+            return {"images": new}
         return {"images": []}
 
     @app.get("/sd-hub-gallery/image{img_path:path}")
-    async def img(img_path: str):
+    async def sendImage(img_path: str):
         fp = Path(img_path)
         if fp.exists():
             return responses.FileResponse(fp, headers=headers)
         raise HTTPException(status_code=404, detail="Image not found")
 
     @app.get("/sd-hub-gallery/thumb/{thumb_name}")
-    async def thumb(thumb_name: str):
+    async def sendThumb(thumb_name: str):
         for f in thumb_dir.iterdir():
             if f.is_dir():
                 t = f / thumb_name
@@ -96,7 +115,7 @@ def GalleryAPI(_: gr.Blocks, app: FastAPI):
 def GalleryTab():
     with gr.TabItem("Gallery", elem_id="sdhub-gallery-tab"):
         with FormRow(equal_height=False, elem_id="sdhub-gallery-image-info-row"):
-            with FormColumn(variant="compact"):
+            with FormColumn(variant="compact", scale=3):
                 image = gr.Image(
                     elem_id="SDHubimgInfoImage",
                     type="pil",
