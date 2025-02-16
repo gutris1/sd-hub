7d93c1cd589ff6a3c852fe50fb93d3f927f9cfe9
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-02-16 12:31:19 +0700

    Update sd-hub-svg.js

--- javascript/sd-hub-svg.js
+++ javascript/sd-hub-svg.js
@@ -89,3 +89,33 @@ var SDHubGallerySpinnerSVG = `
     </g>
   </svg>
 `;
+
+var SDHubGalleryCloseButtonSVG = `
+  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+    width="32px" height="32px" viewBox="0 0 512 512">
+    <path fill="currentColor" d="M330.443 256l136.765-136.765c14.058-14.058 14.058-36.85
+      0-50.908l-23.535-23.535c-14.058-14.058-36.85-14.058-50.908 0L256 181.557L119.235
+      44.792c-14.058-14.058-36.85-14.058-50.908 0L44.792 68.327c-14.058 14.058-14.058
+      36.85 0 50.908L181.557 256L44.792 392.765c-14.058 14.058-14.058 36.85 0 50.908l23.535
+      23.535c14.058 14.058 36.85 14.058 50.908 0L256 330.443l136.765 136.765c14.058 14.058
+      36.85 14.058 50.908 0l23.535-23.535c14.058-14.058 14.058-36.85 0-50.908L330.443 256z"/>
+  </svg>
+`;
+
+var SDHubGalleryPrevButtonSVG = `
+  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+    width="32px" height="32px" viewBox="0 0 24 24">
+    <path
+      d="m4.431 12.822 13 9A1 1 0 0 0 19 21V3a1
+      1 0 0 0-1.569-.823l-13 9a1.003 1.003 0 0 0 0 1.645z"/>
+  </svg>
+`;
+
+var SDHubGalleryNextButtonSVG = `
+<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+  width="32px" height="32px" viewBox="0 0 24 24">
+  <path
+    d="M5.536 21.886a1.004 1.004 0 0 0 1.033-.064l13-9a1
+    1 0 0 0 0-1.644l-13-9A1 1 0 0 0 5 3v18a1 1 0 0 0 .536.886z"/>
+</svg>
+`;
