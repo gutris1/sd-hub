b769968b9c69f70e279da614cef2e4849684a5dc
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-03-06 02:58:31 +0700

    Update install.py

--- install.py
+++ install.py
@@ -1,126 +1,93 @@
 from importlib import metadata
 from packaging import version
-from typing import List, Dict
 from pathlib import Path
 import urllib.request
 import subprocess
 import requests
 import zipfile
 import launch
-import shutil
 import sys
 import os
 
-base = Path(__file__).parent
-req_ = base / 'requirements.txt'
+ORG = '\033[38;5;208m'
+BLU = '\033[38;5;39m'
+RST = '\033[0m'
 
-orange_ = '\033[38;5;208m'
-blue_ = '\033[38;5;39m'
-reset_ = '\033[0m'
+base = Path(__file__).parent
 
 def _exifReader():
-    req1 = base / 'javascript/exif-reader.js'
-    req2 = base / 'javascript/exif-reader-LICENSE'
-    
-    url1 = 'https://raw.githubusercontent.com/mattiasw/ExifReader/main/dist/exif-reader.js'
-    url2 = 'https://raw.githubusercontent.com/mattiasw/ExifReader/main/LICENSE'
-    
-    if not req1.exists():
-        with urllib.request.urlopen(url1) as r, open(req1, 'wb') as o:
-            shutil.copyfileobj(r, o)
-    
-    if not req2.exists():
-        with urllib.request.urlopen(url2) as r, open(req2, 'wb') as o:
-            shutil.copyfileobj(r, o)
+    req = {
+        'javascript/exif-reader.js': 'https://raw.githubusercontent.com/mattiasw/ExifReader/main/dist/exif-reader.js',
+        'javascript/exif-reader-LICENSE': 'https://raw.githubusercontent.com/mattiasw/ExifReader/main/LICENSE'
+    }
+    for path, url in req.items():
+        fp = base / path
+        if not fp.exists():
+            fp.write_bytes(urllib.request.urlopen(url).read())
 
-def _sub(inputs: List[str]) -> bool:
+def _sub(inputs):
     try:
-        subprocess.run(
-            inputs, check=True,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.STDOUT
-        )
-
+        subprocess.run(inputs, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
         return True
-
     except subprocess.CalledProcessError:
         return False
 
-def _check_req(pkg: str, args: str, cmd: str, pkg_list: List[str]) -> None:
+def _check_req(pkg, args, cmd, pkg_list):
     try:
-        subprocess.run(
-            [pkg, args],
-            check=True,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL
-        )
-
+        subprocess.run([pkg, args], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     except FileNotFoundError:
         pkg_list.append(pkg)
         _sub(cmd.split())
 
-def _install_req_1() -> None:
-    reqs = []
-    names = []
+def _install_req_1():
+    reqs, names = [], []
 
-    with open(req_) as file:
-        for pkg in file:
-            pkg = pkg.strip()
-
+    with open(base / 'requirements.txt') as file:
+        for pkg in map(str.strip, file):
             if '==' in pkg:
                 pkg_name, pkg_version = pkg.split('==')
                 try:
-                    _version = metadata.version(pkg_name)
-                    if version.parse(_version) < version.parse(pkg_version):
+                    if version.parse(metadata.version(pkg_name)) < version.parse(pkg_version):
                         reqs.append(pkg)
                         names.append(pkg_name)
-
                 except metadata.PackageNotFoundError:
                     reqs.append(pkg)
                     names.append(pkg_name)
 
-            else:
-                if not launch.is_installed(pkg):
-                    reqs.append(pkg)
-                    names.append(pkg)
+            elif not launch.is_installed(pkg):
+                reqs.append(pkg)
+                names.append(pkg)
 
-        if not sys.platform == 'win32':
-            if not launch.is_installed('aria2'):
-                reqs.append('aria2')
-                names.append('aria2')
+        if sys.platform != 'win32' and not launch.is_installed('aria2'):
+            reqs.append('aria2')
+            names.append('aria2')
 
         if reqs:
-            print(
-                f"Installing SD-Hub requirement: "
-                f"{' '.join(f'{orange_}{pkg}{reset_}' for pkg in names)}"
-            )
-
+            print(f"Installing SD-Hub requirement: {' '.join(f'{ORG}{pkg}{RST}' for pkg in names)}")
             for pkg in reqs:
-                subprocess.run(
-                    [sys.executable, '-m', 'pip', 'install', '-q', pkg]
-                )
+                subprocess.run([sys.executable, '-m', 'pip', 'install', '-q', pkg])
 
-def _install_req_2() -> None:
-    pkg_list: List[str] = []
+def _install_req_2():
+    pkg_list = []
 
     if sys.platform == 'win32':
         aria2_exe = base / 'aria2c.exe'
 
-        if not launch.is_installed('lz4'): pkg_list.append('lz4')
-        if not aria2_exe.exists(): pkg_list.append('aria2')
+        if not launch.is_installed('lz4'):
+            pkg_list.append('lz4')
+        if not aria2_exe.exists():
+            pkg_list.append('aria2')
 
         for pkg_name in pkg_list:
             if pkg_name == 'lz4':
                 subprocess.run([sys.executable, '-m', 'pip', 'install', '-q', 'lz4'])
             elif pkg_name == 'aria2':
                 aria2_url = 'https://github.com/aria2/aria2/releases/download/release-1.37.0/aria2-1.37.0-win-64bit-build1.zip'
+                aria2_zip = base / Path(aria2_url).name
 
                 with requests.get(aria2_url, stream=True) as r:
                     r.raise_for_status()
-                    aria2_zip = base / Path(aria2_url).name
-                    with open(aria2_zip, 'wb') as f:
-                        for chunk in r.iter_content(chunk_size=8192):
-                            f.write(chunk)
+                    aria2_zip.write_bytes(r.content)
 
                 with zipfile.ZipFile(aria2_zip, 'r') as zip_ref:
                     for f in zip_ref.infolist():
@@ -132,38 +99,23 @@ def _install_req_2() -> None:
                 aria2_zip.unlink()
 
     else:
-        env_list: Dict[str, str] = {
+        env_list = {
             'Colab': 'COLAB_JUPYTER_TOKEN',
             'SageMaker Studio Lab': 'SAGEMAKER_INTERNAL_IMAGE_URI',
             'Kaggle': 'KAGGLE_DATA_PROXY_TOKEN'
         }
 
-        env = 'Unknown'
+        env = next((envs for envs, var in env_list.items() if var in os.environ), 'Unknown')
+        pkg_cmds = {'apt': 'update', 'conda': '--version'}
+        pv_lz4 = {'pv': '-V', 'lz4': '-V'}
 
-        for envs, var in env_list.items():
-            if var in os.environ:
-                env = envs
-                break
+        if env in ['Colab', 'Kaggle'] and _sub(['apt', pkg_cmds['apt']]):
+            for pkg, args in pv_lz4.items():
+                _check_req(pkg, args, f"apt -y install {pkg}", pkg_list)
 
-        pkg_cmds: Dict[str, str] = {
-            'apt': 'update',
-            'conda': '--version'
-        }
-
-        pv_lz4: Dict[str, str] = {
-            'pv': '-V',
-            'lz4': '-V'
-        }
-
-        if env in ['Colab', 'Kaggle']:
-            if _sub(['apt', pkg_cmds['apt']]):
-                for pkg, args in pv_lz4.items():
-                    _check_req(pkg, args, f"apt -y install {pkg}", pkg_list)
-
-        elif env == 'SageMaker Studio Lab':
-            if _sub(['conda', pkg_cmds['conda']]):
-                for pkg, args in pv_lz4.items():
-                    _check_req(pkg, args, f"conda install -qyc conda-forge {pkg}", pkg_list)
+        elif env == 'SageMaker Studio Lab' and _sub(['conda', pkg_cmds['conda']]):
+            for pkg, args in pv_lz4.items():
+                _check_req(pkg, args, f"conda install -qyc conda-forge {pkg}", pkg_list)
 
         elif env == 'Unknown':
             if _sub(['apt', pkg_cmds['apt']]):
@@ -176,10 +128,7 @@ def _install_req_2() -> None:
                 print("SD-Hub: Failed to install pv and lz4 in an unknown environment")
 
     if pkg_list:
-        print(
-            f"Installing SD-Hub requirement: "
-            f"{' '.join(f'{blue_}{pkg}{reset_}' for pkg in pkg_list)}"
-        )
+        print(f"Installing SD-Hub requirement: {' '.join(f'{BLU}{pkg}{RST}' for pkg in pkg_list)}")
 
 _exifReader()
 _install_req_1()
