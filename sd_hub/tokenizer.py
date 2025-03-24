03445068aeca33f87c8cc73632b7f4b4f779e1fd
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-03-24 16:12:09 +0700

    Update tokenizer.py

--- sd_hub/tokenizer.py
+++ sd_hub/tokenizer.py
@@ -5,36 +5,34 @@ import json
 Token = Path(basedir()) / '.sd-hub-token.json'
 
 Keys = {
-    'write': 'huggingface-token-write',
-    'read': 'huggingface-token-read',
-    'civitai': 'civitai-api-key'
+    'write': ('huggingface-token-write', 'Huggingface Token (WRITE)'),
+    'read': ('huggingface-token-read', 'Huggingface Token (READ)'),
+    'civitai': ('civitai-api-key', 'Civitai API Key')
 }
 
 def load_token(Tab: str = 'all'):
-    Info = {
-        Keys['write']: 'Huggingface Token (WRITE) loaded',
-        Keys['read']: 'Huggingface Token (READ) loaded',
-        Keys['civitai']: 'Civitai API Key loaded'
-    }
+    i = {k[0]: (f'{k[1]} Loaded', f'{k[1]} Not Found') for k in Keys.values()}
 
     try:
         v = json.loads(Token.read_text(encoding='utf-8'))
     except FileNotFoundError:
-        return '', '', '', f'{Token.name} Not Found', f'{Token.name} Not Found'
+        return '', '', '', f'{Token} Not Found.', f'{Token} Not Found.'
 
-    t = {key: v.get(key, '') for key in Keys.values()}
+    t = {k[0]: v.get(k[0], '') for k in Keys.values()}
 
     if Tab == 'uploader':
-        t[Keys['read']] = ''
-        t[Keys['civitai']] = ''
+        r = [Keys['write'][0]]
+        t[Keys['read'][0]], t[Keys['civitai'][0]] = '', ''
     elif Tab == 'downloader':
-        t[Keys['write']] = ''
+        r = [Keys['read'][0], Keys['civitai'][0]]
+        t[Keys['write'][0]] = ''
+    else:
+        r = [k[0] for k in Keys.values()]
 
-    parts = [Info[key] for key, val in t.items() if val]
-    msg = ', '.join(parts) if parts else "No Token Found"
+    m = ', '.join(i[k][0] if t[k] else i[k][1] for k in r) or 'No Token Found.'
+    print(f'SD-Hub : {m}')
 
-    print(f"SD-Hub : {msg}")
-    return t[Keys['write']], t[Keys['read']], t[Keys['civitai']], msg, msg
+    return t[Keys['write'][0]], t[Keys['read'][0]], t[Keys['civitai'][0]], m, m
 
 def save_token(token1=None, token2=None, token3=None):
     if Token.exists():
@@ -45,8 +43,15 @@ def save_token(token1=None, token2=None, token3=None):
     else:
         v = {}
 
-    for key, token in zip(Keys.values(), [token1, token2, token3]):
-        v[key] = token if token is not None else v.get(key, '')
+    s = []
+    i = {k[0]: k[1] for k in Keys.values()}
 
+    for k, t in zip([Keys['write'][0], Keys['read'][0], Keys['civitai'][0]], [token1, token2, token3]):
+        if t:
+            v[k] = t
+            s.append(i[k])
+
+    m = ', '.join(s) if s else 'No Token Saved.'
     Token.write_text(json.dumps(v, indent=4), encoding='utf-8')
-    return f'Token Saved To: {Token}'
+
+    return f'{m}\nSaved To: {Token}' if s else 'No Token Saved.'
