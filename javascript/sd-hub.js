ab93fdf5b889c483faa14a6d51d601b949d2bb2f
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-04-17 00:26:03 +0700

    Update sd-hub.js

--- javascript/sd-hub.js
+++ javascript/sd-hub.js
@@ -19,7 +19,13 @@ let SDHubLangIndex = {
 
 let SDHubTranslations = {};
 
-onUiLoaded(() => (SDHubTabLoaded(), SDHubTokenBlur(), SDHubEvents(), SDHubUITranslation(), onUiUpdate(SDHubTabChange)));
+onUiLoaded(() => {
+  SDHubTabLoaded();
+  SDHubTokenBlur();
+  SDHubEvents();
+  SDHubUITranslation();
+  onUiUpdate(SDHubTabChange);
+});
 
 function SDHubTabChange() {
   let infoColumn = document.getElementById('SDHub-Gallery-Info-Column');
@@ -345,6 +351,22 @@ function SDHubUITranslation() {
   }
 }
 
+async function SDHubRGBA() {
+  const vars = [
+    { c: '--input-background-fill', to: '--sdhub-gallery-output-bg', a: 0.5 },
+  ];
+
+  const css = await (await fetch('/theme.css')).text();
+  const get = s => Object.fromEntries((css.match(new RegExp(`${s}\\s*{([^}]*)}`, 'm'))?.[1] || '')
+    .split(';').map(l => l.trim().split(':').map(s => s.trim())).filter(([k, v]) => k && v));
+  const toRGBA = (hex, a) => hex && /^#/.test(hex) ? `rgba(${hex.slice(1).match(/.{2}/g).map(v => parseInt(v, 16)).join(',')},${a})` : 'rgba(0,0,0,0)';
+  const r = get(':root'), d = get('.dark'), S = document.createElement('style');
+  vars.forEach(({ c, to, a }) => {
+    S.textContent += `:root { ${to}: ${toRGBA(r[c], a)}; }\n.dark { ${to}: ${toRGBA(d[c], a)}; }\n`;
+  });
+  document.head.appendChild(S);
+}
+
 document.addEventListener('DOMContentLoaded', async function () {
   await new Promise(resolve => (function check() { window.XLSX ? resolve() : setTimeout(check, 50); })());
 
@@ -370,6 +392,7 @@ document.addEventListener('DOMContentLoaded', async function () {
 
       SDHubTextEditorGalleryScrollBar();
       SDHubGalleryDOMLoaded();
+      SDHubRGBA();
     }
   } catch (err) { console.error('XLSX Error:', err); }
 });
