0912f01053394ad98f6017a618df51baa371bd3e
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-05-22 18:46:48 +0700

    Update sd-hub-svg.js

--- javascript/sd-hub-svg.js
+++ javascript/sd-hub-svg.js
@@ -104,18 +104,14 @@ var SDHubGalleryCloseButtonSVG = `
 var SDHubGalleryPrevButtonSVG = `
   <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
     width="32px" height="32px" viewBox="0 0 24 24">
-    <path
-      d="m4.431 12.822 13 9A1 1 0 0 0 19 21V3a1
-      1 0 0 0-1.569-.823l-13 9a1.003 1.003 0 0 0 0 1.645z"/>
+    <path d="m4.431 12.822 13 9A1 1 0 0 0 19 21V3a1 1 0 0 0-1.569-.823l-13 9a1.003 1.003 0 0 0 0 1.645z"/>
   </svg>
 `;
 
 var SDHubGalleryNextButtonSVG = `
   <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
     width="32px" height="32px" viewBox="0 0 24 24">
-    <path
-      d="M5.536 21.886a1.004 1.004 0 0 0 1.033-.064l13-9a1
-      1 0 0 0 0-1.644l-13-9A1 1 0 0 0 5 3v18a1 1 0 0 0 .536.886z"/>
+    <path d="M5.536 21.886a1.004 1.004 0 0 0 1.033-.064l13-9a1 1 0 0 0 0-1.644l-13-9A1 1 0 0 0 5 3v18a1 1 0 0 0 .536.886z"/>
   </svg>
 `;
 
