41ef39c47df597511ae86b10e5b69e61d2b13f34
gutris1 <132797949+gutris1@users.noreply.github.com>
2025-05-26 16:50:38 +0700

    Update sd-hub-svg.js

--- javascript/sd-hub-svg.js
+++ javascript/sd-hub-svg.js
@@ -1,17 +1,14 @@
 var SDHubGalleryImageButtonSVG = `
   <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none'
     width='30px' height='30px'>
-    <path d='M4 6H20M4 12H20M4 18H20' stroke='currentColor' stroke-width='2'
-      stroke-linecap='round' stroke-linejoin='round'/>
+    <path d='M4 6H20M4 12H20M4 18H20' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/>
   </svg>
 `;
 
 var SDHubGalleryDLSVG = `
   <svg class='sdhub-gallery-cm-svg' xmlns='http://www.w3.org/2000/svg'
     width='32' height='32' viewBox='0 0 32 32'>
-    <path fill='currentColor' stroke='currentColor' stroke-width='1.8'
-      d='M26 24v4H6v-4H4v4a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2v-4zm0-10
-      l-1.41-1.41L17 20.17V2h-2v18.17l-7.59-7.58L6 14l10 10l10-10z'/>
+    <path fill='currentColor' stroke='currentColor' stroke-width='1.8' d='M26 24v4H6v-4H4v4a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2v-4zm0-10 l-1.41-1.41L17 20.17V2h-2v18.17l-7.59-7.58L6 14l10 10l10-10z'/>
   </svg>
 `;
 
@@ -101,14 +98,14 @@ var SDHubGalleryCloseButtonSVG = `
   </svg>
 `;
 
-var SDHubGalleryPrevButtonSVG = `
+var SDHubGalleryLeftArrowSVG = `
   <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
     width="32px" height="32px" viewBox="0 0 24 24">
     <path d="m4.431 12.822 13 9A1 1 0 0 0 19 21V3a1 1 0 0 0-1.569-.823l-13 9a1.003 1.003 0 0 0 0 1.645z"/>
   </svg>
 `;
 
-var SDHubGalleryNextButtonSVG = `
+var SDHubGalleryRightArrowSVG = `
   <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
     width="32px" height="32px" viewBox="0 0 24 24">
     <path d="M5.536 21.886a1.004 1.004 0 0 0 1.033-.064l13-9a1 1 0 0 0 0-1.644l-13-9A1 1 0 0 0 5 3v18a1 1 0 0 0 .536.886z"/>
